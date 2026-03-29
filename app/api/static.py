295bd3836d1dd4dfc0bb2cf815c2ef60093748f1
hu <hu__fx@163.com>
2026-03-29 23:59:02 +0800

    health

--- app/api/static.py
+++ app/api/static.py
@@ -1,5 +1,5 @@
 import os
-from flask import Blueprint, send_from_directory
+from flask import Blueprint, send_from_directory,jsonify
 from app.core.config import BASE_DIR
 
 static_bp = Blueprint('static', __name__)
@@ -10,6 +10,15 @@ current_dir = str(BASE_DIR)
 @static_bp.route('/assets/<filename>')
 def custom_static(filename):
     return send_from_directory(os.path.join(current_dir, 'assets'), filename)
+
+@static_bp.route('/health')
+def health():
+    return jsonify(
+                {
+                    "code": 200,
+                    "msg": None,
+                    "data": "ok"
+                }), 200
 
 # 处理 favicon.ico 静态资源（未来打包用）
 @static_bp.route('/favicon.ico')
